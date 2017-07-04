6b096acbf78311a03f2bcda81a3b25f1dc577919
Camilo A. Ramirez A <Camilo.1219@hotmail.com>
2017-07-04 18:28:04 -0500

    Se termino el video 30 de la carpeta 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { routing, appRoutingProviders } from './app.routing';
 import { AppComponent } from './app.component';
 import { FrutaComponent } from './fruta/fruta.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { ContactoComponent} from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadoComponent,
-    FrutaComponent
+    FrutaComponent,
+    ContactoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
