b38d29e36f5f237e61ac9b6173712940b689ddf6
Camilo A. Ramirez A <Camilo.1219@hotmail.com>
2017-07-08 18:51:39 -0500

    Se termino el video 45 de la carpeta 10

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Coche } from './coche';
+import {PeticionesService} from '../services/peticiones.service';
 
 @Component({
 	selector: 'coches',
-	templateUrl: './coches.component.html'
+	templateUrl: './coches.component.html',
+	providers: [PeticionesService]
 })
 
 export class CochesComponent{
@@ -11,12 +13,18 @@ export class CochesComponent{
 	public coche: Coche;
 	public coches:Array<Coche>;
 
-	constructor(){
+	constructor(
+			private _peticionesService: PeticionesService
+		){
 		this.coche = new Coche("","","");
 		this.coches = [
 			new Coche("Seat Panda","120","Blanco"),
 			new Coche("Renault","110","Azul")
 		];
+	}
+
+	ngOnInit(){
+		console.log(this._peticionesService.getPrueba());
 	}
 
 	onSubmit(){
